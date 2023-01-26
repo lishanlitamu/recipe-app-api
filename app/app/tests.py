4aa93069b15d4629669245ecb7e07f395e15f081
Mia Li <miali@Mias-MacBook-Air.local>
2023-01-26 09:51:42 -0600

    install psycopg2 to connect postgresql to database

---
+++ app/app/tests.py
@@ -0,0 +1,22 @@
+"""
+Sample tests
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """Test the calc module."""
+
+    def test_add_numbers(self):
+        res = calc.add(3, 4)
+
+        self.assertEqual(res, 7)
+
+
+    def test_subtract_numbers(self):
+        res = calc.subtract(3, 10)
+
+        self.assertEqual(res, 7)
+
