7a08f0cb4d6617c49285488152e31625c5a1a571
Mia Li <miali@Mias-MacBook-Air.local>
2023-01-27 14:54:51 -0600

    fixed lint errors

--- app/app/tests.py
+++ app/app/tests.py
@@ -7,8 +7,8 @@ from django.test import SimpleTestCase
 from app import calc
 
 class CalcTests(SimpleTestCase):
-    """Test the calc module."""
 
+    """Test the calc module."""
 
     def test_add_numbers(self):
         res = calc.add(3, 4)
--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -10,7 +10,6 @@ from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
 
-
     """Django command to wait for database."""
 
     def handle(self, *args, **options):
